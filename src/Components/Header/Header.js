e34c0b5809f9a1c3d9b49ed5be4969c5b74ebc95
Dawidm213 <dawid.micelski@gmail.com>
2022-09-11 12:29:59 +0200

    commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,7 +7,7 @@ export const Header = () => {
         <Container>
             <h1>ChartWebsite</h1>
             <ul>
-                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/https://dawidm213.github.io/pie_chart'>Home</Link></li>
                 <li><Link to='/Charts'>Charts</Link></li>
                 <li><Link to='/About'>About</Link></li>
             </ul>
